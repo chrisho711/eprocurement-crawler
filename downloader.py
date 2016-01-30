5579e8902ff3db64362f693be7040926274530b2
Yu-chun Huang <code@yullage.com>
2016-01-30 16:08:29 +0800

    Initial commit.

---
+++ downloader.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+import os
+import requests
+import logging
+import time
+import re
+import random
+from optparse import OptionParser
+from bs4 import BeautifulSoup
+
+_ERRCODE_FILENAME = 3
+_ERRCODE_DIR = 4
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def parse_args():
+    p = OptionParser()
+    p.add_option('-f', '--list_filename', action='store',
+                 dest='list_filename', type='string', default='')
+    p.add_option('-d', '--directory', action='store',
+                 dest='directory', type='string', default='bid_detail')
+    return p.parse_args()
+
+
+if __name__ == '__main__':
+    options, remainder = parse_args()
+
+    bid_list = options.list_filename.strip()
+    if not bid_list:
+        logger.error('Invalid bid list filename.')
+        quit(_ERRCODE_FILENAME)
+
+    directory = options.directory.strip()
+    if directory:
+        try:
+            os.makedirs(directory)
+        except OSError:
+            if not os.path.isdir(directory):
+                logger.error('Fail to create directory.')
+                quit(_ERRCODE_DIR)
+
+    with open(bid_list, 'r') as f:
+        for line in f.readlines():
+            page_link = line.strip()
+
+            m = re.match(r'([^ ]+)pkAtmMain=(?P<pkAtmMain>\w+)&tenderCaseNo=(?P<tenderCaseNo>\w+)', page_link)
+            pkAtmMain = m.group('pkAtmMain')
+            tenderCaseNo = m.group('tenderCaseNo')
+            filename = "%s_%s" % (pkAtmMain, tenderCaseNo)
+
+            request_get = requests.get(page_link)
+            response = request_get.text.encode('utf8')
+
+            soup = BeautifulSoup(''.join(response), 'lxml')
+            print_area = soup.find('div', {"id": "printArea"})
+
+            with open('{}/{}.txt'.format(directory, filename), 'w') as bid_detail:
+                bid_detail.write(print_area.prettify("utf-8"))
+                logger.info('Writing bid detail (pkAtmMain: {}, tenderCaseNo: {})'.format(pkAtmMain, tenderCaseNo))
+
+            time.sleep(random.randint(0, 2))  # Prevent from being treated as a DDOS attack
